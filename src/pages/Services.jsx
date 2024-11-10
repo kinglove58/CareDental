b0ee9eb19f07377a4f857169a1d6c54856d17ff7
jeremiah <bankolejeremiahadeoye@gmail.com>
2024-11-10 17:36:17 +0100

    done with the pages

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Section1 from '../components/ServicesComponent/Section1'
 import Section2 from '../components/ServicesComponent/Section2'
+import { Link } from 'react-router-dom'
 
 const Services = () => {
   return (
@@ -8,7 +9,9 @@ const Services = () => {
         <Section1 />
         <Section2 />
         <div className="px-4 md:px-24 lg:px-36">
+          <Link to={"/contactus"}>
             <button className='min-h-[55px] mb-4 py-2 px-8 bg-blue-800 text-white font-semibold border border-blue-800 hover:bg-white hover:text-blue-800 transition duration-500'>Contact Us to Start Saving</button>
+          </Link>
         </div>
     </div>
   ) 
