0e765053bacc2a3fa66a1566e177477c3428b137
kinglove58 <obafemielijahsunday@gmail.com>
2024-11-10 20:08:12 +0100

    refactor Footer component: update link text and add feedback link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,62 +3,48 @@ import { Link } from "react-router-dom";
 import logo from "/images/logo/care_dental_logo.png";
 
 const Footer = () => {
-<<<<<<< HEAD
   return (
-    <div className="min-h-[20vh] px-6 py-12 flex flex-col lg:flex-row justify-between gap-10 items-start md:items-center text-[#002F6C] font-semibold bg-gray-400">
+    <div className="min-h-[20vh] px-6 py-12 flex flex-col lg:flex-row justify-between gap-10 items-start md:items-center text-white font-semibold bg-gray-800">
       <img src={logo} alt="Care dental logo" className="w-32 h-auto" />
       <ul className="flex flex-col lg:flex-row items-start md:items-center gap-4">
         <li>
-          <Link to={"/"} className="hover:underline">
+          <Link to="/" className="hover:underline">
             HOME
           </Link>
         </li>
         <li>
-          <Link to={"/services"} className="hover:underline">
+          <Link to="/services" className="hover:underline">
             SAVING
           </Link>
         </li>
         <li>
-          <Link to={"/join"} className="hover:underline">
+          <Link to="/join" className="hover:underline">
             JOIN/RENEW
           </Link>
         </li>
         <li>
-          <Link to={"/contactus"} className="hover:underline">
+          <Link to="/contactus" className="hover:underline">
             CONTACT US
           </Link>
         </li>
         <li>
-          <Link to={"/terms_condition"} className="hover:underline">
+          <Link to="/terms_condition" className="hover:underline">
             TERMS & CONDITIONS
           </Link>
         </li>
         <li>
-          <Link to={"/privacy_policy"} className="hover:underline">
+          <Link to="/privacy_policy" className="hover:underline">
             PRIVACY POLICY
+          </Link>
+        </li>
+        <li>
+          <Link to="/survey" className="hover:underline">
+            GIVE FEEDBACK
           </Link>
         </li>
       </ul>
     </div>
   );
 };
-=======
-
-    return (
-        <div className='min-h-[20vh] px-6 py-12 flex flex-col lg:flex-row justify-between gap-10 items-start md:items-center text-white font-semibold bg-gray-800'>
-            <img src={logo} alt="Care dental logo" className='w-32 h-auto' />
-            <ul className='flex flex-col lg:flex-row items-start md:items-center gap-4'>
-                <li><Link to={"/"} className='hover:underline'>HOME</Link></li>
-                <li><Link to={"/services"} className='hover:underline'>OUR SERVICES</Link></li>
-                <li><Link to={"/join"} className='hover:underline'>JOIN/RENEW</Link></li>
-                <li><Link to={"/contactus"} className='hover:underline'>CONTACT US</Link></li>
-                <li><Link to={"/terms_condition"} className='hover:underline'>TERMS & CONDITIONS</Link></li>
-                <li><Link to={"/privacy_policy"} className='hover:underline'>PRIVACY POLICY</Link></li>
-                <li><Link to={"/survey"} className='hover:underline'>GIVE FEEDBACK</Link></li>
-            </ul>
-        </div>
-    )
-}
->>>>>>> 7efc59771b8afdda6f334050d2d9cb96cb8ef618
 
 export default Footer;
